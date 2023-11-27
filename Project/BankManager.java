7884d97e34068bb7c61a8addcb4eb8dccf4ba7a3
Danielb232 <109778178+Danielb232@users.noreply.github.com>
2023-11-26 23:50:30 -0800

    work

--- Project/BankManager.java
+++ Project/BankManager.java
@@ -1,4 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BankManager {
 
-    
+    private static Scanner scnr = new Scanner(System.in);
+
+    public static void loginToAcc(ArrayList<BankManager> accounts, Scanner scnr) {
+        System.out.println("Please enter your username: ");
+        String userName = scnr.nextLine();
+
+        System.out.println("Please enter your password ");
+        String passWord = scnr.nextLine();
+
+        for(Customer i : accounts) {
+            if(!(i.getUserName().equals(userName)) || (!(i.getPassword().equals(passWord)))) {
+                System.out.print("Username/Password doesn't exist, try again");
+                return;
+            }
+
+        }
+
+    }
 }
--- Project/Customer.java
+++ Project/Customer.java
@@ -1,5 +1,54 @@
 public class Customer {
-    ASDFA
-    //hELLO
     
+
+	private String name;
+    private String password;
+    private String UserName;
+    private double balance;
+    private int accountType;
+    
+    
+    public Customer(String newUser, String newPassword, String newName, int newBalance, int newAccountType){
+        this.name = newName;
+        this.password = newPassword;
+        this.UserName = newUser;
+        this.balance = newBalance;
+        this.accountType = newAccountType;
+    }
+    
+    
+    
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUserName() {
+        return UserName;
+    }
+    public void setUserName(String userName) {
+        this.UserName = userName;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public int getAccountType() {
+        return accountType;
+    }
+    public void setAccountType(int accountType) {
+        this.accountType = accountType;
+    }
+    
+
 }
